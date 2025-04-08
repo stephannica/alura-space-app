a3bf7070886b49667c1f4e4b8af7cfbe9e7179d5
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-08 17:13:20 -0300

    feat: add inputText component to Header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import InputText from "../InputText";
+
+const StyledHeader = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
+`
+
+function Header() {
+    return (
+        <StyledHeader>
+            <img src="/img/logo.png" alt="SpaceAppLogo" />
+            <InputText/>
+        </StyledHeader>
+    )
+}
+
+export default Header;
