aabda97afc34a538deb86cfed51646ee42e40051
Stephanni Cavalcante <stephanni.cavalcante.rocha@gmail.com>
2025-04-30 15:27:14 -0300

    feat: add header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import InputText from "../InputText";
+
+const StyledHeader = styled.header`
+    padding: 60px 0;
+    display: flex;
+    justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
+`
+
+function Header({ filter, setFilter }) {
+    return (
+        <StyledHeader>
+            <img src="/img/logo.png" alt="SpaceAppLogo" />
+            <InputText filter={filter} setFilter={setFilter}/>
+        </StyledHeader>
+    )
+}
+
+export default Header;
